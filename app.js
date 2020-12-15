2a5f66e8a05738a50e1915fa38deea85386deef0
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-15 14:23:58 +0630

    Integrating Express

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const port = 8000;
+
+app.get("/", (req, res) => {
+    res.send("Hello, world!");
+});
+
+app.post("/", (req, res) => {
+    res.status(200).json({
+        message: "Hello, Express",
+    });
+});
+// Starting server
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+});
