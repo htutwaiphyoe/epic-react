eb9324eca8d04a074cc46c552263086b85543684
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-15 18:23:22 +0630

    API for DELETE single tour

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = 8000;
 // body parser
 app.use(express.json());
 // Top level code
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, "utf8"));
+let tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, "utf8"));
 
 // API
 
@@ -103,6 +103,37 @@ app.patch("/api/v1/tours/:id", (req, res) => {
                 data: {
                     tour,
                 },
+            });
+        }
+    );
+});
+
+// Delete a single tour
+app.delete("/api/v1/tours/:id", (req, res) => {
+    const tour = tours.find((tour) => tour.id === +req.params.id);
+    if (!tour) {
+        return res.status(404).json({
+            status: "fail",
+            message: "Tour not found",
+        });
+    }
+    tours = tours.filter((tour) => tour.id !== +req.params.id);
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        "utf-8",
+        (err) => {
+            if (err) {
+                return res.status(500).json({
+                    status: "error",
+                    error: {
+                        message: "Server error",
+                    },
+                });
+            }
+            res.status(204).json({
+                status: "success",
+                data: null,
             });
         }
     );
