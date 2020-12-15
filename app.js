9c6f10cf2688cd81e5f60438dcc8d4dcffbb0509
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-15 16:49:16 +0630

    API for POST new tour

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+// Middleware
+// body parser
+app.use(express.json());
+// Top level code
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, "utf8"));
+
+// API
+
+// GET All Tours
 app.get("/api/v1/tours", (req, res) => {
     res.status(200).json({
         status: "success",
@@ -13,6 +21,36 @@ app.get("/api/v1/tours", (req, res) => {
             tours,
         },
     });
+});
+
+// Create New Tour
+app.post("/api/v1/tours", (req, res) => {
+    const tour = {
+        id: tours.length,
+        ...req.body,
+    };
+    tours.push(tour);
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simplejson`,
+        JSON.stringify(tours),
+        "utf-8",
+        (err) => {
+            if (err) {
+                res.status(500).json({
+                    status: "error",
+                    error: {
+                        message: "Server error",
+                    },
+                });
+            }
+            res.status(200).json({
+                status: "success",
+                data: {
+                    tour,
+                },
+            });
+        }
+    );
 });
 // Starting server
 app.listen(port, () => {
