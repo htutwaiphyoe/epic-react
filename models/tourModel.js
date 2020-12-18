08a4a48e6f612f93eba4fb30963eaac3a6cdf9b1
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-18 06:54:43 +0630

    Modeling tour schema

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,19 +5,52 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, "Missing name"],
         unique: true,
+        trim: true,
     },
     duration: {
         type: Number,
         required: [true, "Missing duration"],
     },
+    maxGroupSize: {
+        type: Number,
+        required: [true, "Missing maxGroupSize"],
+    },
+    difficulty: {
+        type: String,
+        required: [true, "Missing difficulty"],
+    },
+    ratingsAverage: {
+        type: Number,
+        default: 4.5,
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0,
+    },
     price: {
         type: Number,
         required: [true, "Missing price"],
     },
-    rating: {
-        type: Number,
-        default: 4.5,
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        required: [true, "Missing summary"],
+        trim: true,
     },
+    description: {
+        type: String,
+        trim: true,
+    },
+    imageCover: {
+        type: String,
+        required: [true, "Missing imageCover"],
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    startDates: [Date],
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
