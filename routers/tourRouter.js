6064ed5d2b77bfbeebd497c81fe90b5e3a8edda3
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-19 09:48:51 +0630

    API for monthly plan with aggregation pipeline

--- routers/tourRouter.js
+++ routers/tourRouter.js
@@ -6,9 +6,15 @@ const tourController = require("../controllers/tourController");
 const router = express.Router();
 // router.param("id", tourController.checkId);
 router.route("/top-5").get(tourController.aliasTop5, tourController.getAllTours);
+
 router.route("/cheapest").get(tourController.aliasCheapest, tourController.getAllTours);
+
 router.route("/tour-stats").get(tourController.getTourStats);
+
+router.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
+
 router.route("/").get(tourController.getAllTours).post(tourController.addNewTour);
+
 router
     .route("/:id")
     .get(tourController.getSingleTour)
