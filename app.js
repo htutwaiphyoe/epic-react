baf4e60477338eda5de70f15be0d214b1e5920e3
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-22 16:33:36 +0630

    Custom Error Class

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 // own modules
 const tourRouter = require("./routers/tourRouter");
 const userRouter = require("./routers/userRouter");
+const AppError = require("./utils/AppError");
+const globalErrorController = require("./controllers/errorController");
 
 const app = express();
 
@@ -24,20 +26,9 @@ app.use("/api/v1/users", userRouter);
 
 // Unhandled routes middleware
 app.all("*", (req, res, next) => {
-    const err = new Error(`Can't find ${req.originalUrl} on this server.`);
-    err.statusCode = 404;
-    err.status = "fail";
-    next(err);
+    next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404));
 });
 
 // Global Error Handling Middleware
-app.use((err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || "error";
-
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    });
-});
+app.use(globalErrorController);
 module.exports = app;
