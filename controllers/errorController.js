2a456428fa09891c6f8967dc3b5bc1769502648b
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-22 18:34:56 +0630

    Handling validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -4,6 +4,7 @@ const AppError = require("../utils/AppError");
 const sendDevError = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
+        name: err.name,
         error: err,
         message: err.message,
         stack: err.stack,
@@ -27,18 +28,25 @@ const sendProdError = (err, res) => {
 
 const handleCastError = (err) => new AppError(`Invalid ${err.path}: ${err.value}`, 400);
 
-const handleDuplicateKeyError = (err) =>
-    new AppError(
-        `${Object.keys(err.keyValue).join(" ")}: ${err.keyValue.name} is already in use`,
-        400
-    );
+const handleDuplicateKeyError = (err) => {
+    const message = `${Object.keys(err.keyValue).join(" ")}: ${
+        err.keyValue.name
+    } is already in use`;
 
+    return new AppError(message, 400);
+};
+
+const handleValidationError = (err) => {
+    const message = Object.values(err.errors)
+        .map((val) => val.message)
+        .join(". ");
+    return new AppError(message, 400);
+};
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || "error";
 
     if (process.env.NODE_ENV === "development") {
-        // console.log(err.code, err.keyValue.name);
         sendDevError(err, res);
     } else if (process.env.NODE_ENV === "production") {
         if (err.name === "CastError") {
@@ -46,6 +54,9 @@ module.exports = (err, req, res, next) => {
         }
         if (err.code === 11000) {
             err = handleDuplicateKeyError(err);
+        }
+        if (err.name === "ValidationError") {
+            err = handleValidationError(err);
         }
         sendProdError(err, res);
     }
