c3c83af544c0431d957c0f43e64b02b31d4c66a1
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-22 17:38:24 +0630

    Distinguishing errors into Development & Production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,31 @@
+const sendDevError = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+    });
+};
+
+const sendProdError = (err, res) => {
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+    } else {
+        console.error(err);
+        res.status(500).json({
+            status: "error",
+            message: "Oops! Something went wrong.💥",
+        });
+    }
+};
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || "error";
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    });
+    if (process.env.NODE_ENV === "development") {
+        sendDevError(err, res);
+    } else if (process.env.NODE_ENV === "production") {
+        sendProdError(err, res);
+    }
 };
