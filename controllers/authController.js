54fa956b7195feba32a17b14957db310e8a19aea
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-24 14:34:39 +0630

    Sign Up

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,24 @@
+// third-party modules
+const jwt = require("jsonwebtoken");
 // own modules
 const User = require("../models/userModel");
 const catchError = require("../utils/catchError");
+const AppError = require("../utils/AppError");
 
 exports.signup = catchError(async (req, res, next) => {
-    const user = await User.create(req.body);
+    const user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        comfirmPassword: req.body.comfirmPassword,
+    });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
     res.status(201).json({
         status: "success",
+        token,
         data: {
             user,
         },
