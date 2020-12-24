b56bfd669612d96a618bcef044c655b847f4646c
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-24 06:31:29 +0630

    Modeling User

---
+++ models/userModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        lowercase: true,
+        required: [true, "Missing name"],
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: [true, "Missing email"],
+        lowercase: true,
+    },
+    photo: {
+        type: String,
+        default: "asdfsd",
+    },
+    password: {
+        type: String,
+        trim: true,
+        minLength: 8,
+        required: [true, "Missing password"],
+    },
+    comfirmPassword: {
+        type: String,
+        required: [true, "Missing confirm password"],
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
