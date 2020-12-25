174063a33e7cb5e2cd3733bae8e409b1dac0e5f6
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-25 14:15:59 +0630

    Request rate limiting

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // third-party modules
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+
 // own modules
 const tourRouter = require("./routers/tourRouter");
 const userRouter = require("./routers/userRouter");
@@ -10,6 +12,14 @@ const globalErrorController = require("./controllers/errorController");
 const app = express();
 
 // Middleware
+// rate limiter
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many requests, please try again in an hour",
+});
+
+app.use("/api", limiter);
 // logger
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
