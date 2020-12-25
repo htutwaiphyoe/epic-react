254ee867cfc60575551203af1229eda97941742e
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-25 13:55:20 +0630

    Storing cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,6 +16,20 @@ const getToken = (id) =>
 
 const sendResponseWithToken = (user, statusCode, res, data = undefined) => {
     const token = getToken(user._id);
+
+    // send cookie
+    const cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly: true,
+    };
+    if (process.env.NODE_ENV === "production") {
+        cookieOptions.secure = true;
+    }
+    res.cookie("jwt", token, cookieOptions);
+
+    // delete password
+    user.password = undefined;
+    // send response
     res.status(statusCode).json({
         status: "success",
         token,
