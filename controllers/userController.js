ce7eb7c262941220dd941ea0275826a5a9720cc7
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-25 11:45:09 +0630

    Update User data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,44 @@
+// own modules
+const User = require("../models/userModel");
+const catchError = require("../utils/catchError");
+const AppError = require("../utils/AppError");
+
+const filterBody = (body, ...allowedFields) => {
+    const newBody = {};
+    Object.keys(body).forEach((field) => {
+        if (allowedFields.includes(field)) {
+            newBody[field] = body[field];
+        }
+    });
+    return newBody;
+};
+exports.updateMe = catchError(async (req, res, next) => {
+    // check password includes
+    if (req.body.password || req.body.confirmPassword) {
+        return next(new AppError("Password can't be updated here. Try another", 400));
+    }
+
+    // filter body
+    const body = filterBody(req.body, "name", "email");
+    if (Object.keys(body).length === 0) {
+        return next(new AppError("Please fill the form", 400));
+    }
+
+    // update user
+    const user = await User.findByIdAndUpdate(req.user._id, body, {
+        new: true,
+        runValidators: true,
+    });
+
+    // send response
+    res.status(200).json({
+        status: "success",
+        data: {
+            user,
+        },
+    });
+});
+
 exports.getAllUsers = (req, res) => {
     res.status(500).json({
         status: "error",
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -11,6 +11,7 @@ router.post("/login", authController.login);
 router.post("/forgot-password", authController.forgotPassword);
 router.patch("/reset-password/:token", authController.resetPassword);
 router.patch("/update-password", authController.protect, authController.updatePassword);
+router.patch("/update-me", authController.protect, userController.updateMe);
 
 router.route("/").get(userController.getAllUsers).post(userController.addNewUser);
 router
