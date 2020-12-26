29314cdce371787b5fea04f06433acfe156fab8a
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-26 17:16:24 +0630

    #add: review model

---
+++ models/reviewModel.js
@@ -0,0 +1,40 @@
+// third-party modules
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+    {
+        review: {
+            type: String,
+            trim: true,
+            required: [true, "Missing review"],
+        },
+        rating: {
+            type: Number,
+            required: [true, "Missing review rating"],
+            min: 1,
+            max: 5,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
+        tour: {
+            type: mongoose.Schema.ObjectId,
+            ref: "Tour",
+            required: [true, "Missing tour that was reviewed"],
+        },
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: "User",
+            required: [true, "Missing user who reviewed"],
+        },
+    },
+    {
+        toObject: { virtuals: true },
+        toJSON: { virtuals: true },
+    }
+);
+
+const Review = mongoose.model("Review", reviewSchema);
+
+module.exports = Review;
