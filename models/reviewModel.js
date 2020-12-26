6f77bf9b57f902110dcf133e305e3d6a3fb595dc
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-26 09:29:50 +0630

    #add: populating tour & user in reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -35,6 +35,18 @@ const reviewSchema = new mongoose.Schema(
     }
 );
 
+// query middlewares
+// populate user and tour
+reviewSchema.pre(/^find/, function (next) {
+    this.populate({
+        path: "tour",
+        select: "name",
+    }).populate({
+        path: "user",
+        select: "name photo",
+    });
+    next();
+});
 const Review = mongoose.model("Review", reviewSchema);
 
 module.exports = Review;
