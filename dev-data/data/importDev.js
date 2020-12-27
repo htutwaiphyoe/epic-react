acfcad6e108de7e867ba89cea7d2e3d0eb7342e6
htutwaiphyoe <htutwaiphyoe@gmail.com>
2020-12-27 06:30:30 +0630

    #add: import all data

--- dev-data/data/importDev.js
+++ dev-data/data/importDev.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
 const Tour = require("../../models/tourModel");
+const User = require("../../models/userModel");
+const Review = require("../../models/reviewModel");
 
 dotenv.config({ path: "./.env" });
 
@@ -24,10 +26,14 @@ mongoose.connection.on("error", (err) => {
 });
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf8"));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf8"));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`, "utf8"));
 
 const importData = async () => {
     try {
         await Tour.create(tours);
+        await User.create(users, { validateBeforeSave: false });
+        await Review.create(reviews);
         console.log("Data imported successfully");
     } catch (err) {
         console.log(err);
@@ -38,6 +44,8 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Tour.deleteMany({});
+        await User.deleteMany({});
+        await Review.deleteMany({});
         console.log("Data deleted successfully");
     } catch (err) {
         console.log(err);
--- models/userModel.js
+++ models/userModel.js
@@ -23,7 +23,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        enum: ["user", "admin"],
+        enum: ["user", "guide", "lead guide", "admin"],
         default: "user",
     },
     photo: {
