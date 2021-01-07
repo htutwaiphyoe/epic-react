753997864220f22e0c0dd0ae83c229eff4dc2e34
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-01-07 09:07:21 +0630

    #add: building tour detail page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -12,7 +12,10 @@ exports.getOverview = catchError(async (req, res) => {
 
 exports.getTourDetail = catchError(async (req, res) => {
     const { slug } = req.params;
-    const tour = await Tour.findOne({ slug }).populate("reviews");
+    const tour = await Tour.findOne({ slug }).populate({
+        path: "reviews",
+        fields: "review rating user",
+    });
 
     res.status(200).render("tour", {
         title: tour.name,
